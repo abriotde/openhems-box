c6aae6e45393785713239a54a91db4a6d8345cdc
alberic <alberic.briot@osiris-team.fr>
2024-09-30 14:03:32 +0200

    Add bottom ventilation holes

--- scripts/git_export.sh
+++ scripts/git_export.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
-cd ../OpenHEMS-server
-unzip -n ../OpenHEMS-server.FCStd
+
+SCRIPT_PATH=`dirname $0`
+ZIP_FOLDER=$SCRIPT_PATH/../OpenHEMS-server
+FCSTD_PATH="../OpenHEMS-server.FCStd"
+cd $ZIP_FOLDER
+ZIP_FOLDER=`pwd`
+echo "Path is $ZIP_FOLDER"
+unzip -n $FCSTD_PATH
 
